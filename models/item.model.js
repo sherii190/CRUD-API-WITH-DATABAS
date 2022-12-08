8d74ad143f6067659bbe90827ed780072b158319
Shehryar Hassan <phonetech195@gmail.com>
2022-12-08 21:06:53 +0100

    Create Schema Ready, Item.model.js file ready

--- models/item.model.js
+++ models/item.model.js
@@ -15,3 +15,16 @@ const Schema = mongoose.Schema;
 //     { "name": "Sandwich", "description": "A delicious sandwich", "price": 5.99 },
 //     { "name": "Ice Cream", "description": "A delicious ice cream", "price": 2.99 },
 // ]
+
+// Create Schema
+const itemSchema = new Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+}, {
+  timestamps: true,
+  _id: true,
+});
+
+const Item = mongoose.model("Item", itemSchema);
+module.exports = Item;
