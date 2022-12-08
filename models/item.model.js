a674a6b9c3a453408b0d7a0a416fc3382ed3c4b0
Shehryar Hassan <phonetech195@gmail.com>
2022-12-08 21:06:07 +0100

    MongoDB ans Schema for Item Model with sample data

---
+++ models/item.model.js
@@ -0,0 +1,17 @@
+// Description: This file contains the schema for the item model
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+//! Sample Data
+// const food_items = [
+//     { "name": "Burger", "description": "A delicious burger", "price": 5.99 },
+//     { "name": "Pizza", "description": "A delicious pizza", "price": 7.99 },
+//     { "name": "Hot Dog", "description": "A delicious hot dog", "price": 3.99 },
+//     { "name": "Fries", "description": "A delicious fries", "price": 2.99 },
+//     { "name": "Salad", "description": "A delicious salad", "price": 4.99 },
+//     { "name": "Sushi", "description": "A delicious sushi", "price": 6.99 },
+//     { "name": "Taco", "description": "A delicious taco", "price": 3.99 },
+//     { "name": "Burrito", "description": "A delicious burrito", "price": 4.99 },
+//     { "name": "Sandwich", "description": "A delicious sandwich", "price": 5.99 },
+//     { "name": "Ice Cream", "description": "A delicious ice cream", "price": 2.99 },
+// ]
